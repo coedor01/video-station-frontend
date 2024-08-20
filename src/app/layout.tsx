5a8a0b785c7153fb459d2153955cc8851ae23daa
coedor01 <135929935+coedor01@users.noreply.github.com>
2024-08-20 12:48:35 +0800

    feat: 导入next-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { NextUIProvider } from "@nextui-org/react";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html>
+      <body>
+        <NextUIProvider>
+          {children}
+        </NextUIProvider>
+      </body>
     </html>
   );
 }
